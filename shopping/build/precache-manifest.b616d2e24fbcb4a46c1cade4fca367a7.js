8d8edcc5a615a4ef27e8d280a83eb26e1ecd8db0
Karen Bao <karenbao2021@u.northwestern.edu>
2020-01-16 20:16:48 -0600

    another commit

---
+++ shopping/build/precache-manifest.b616d2e24fbcb4a46c1cade4fca367a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6b4dcce33d2245091f5c311d1442c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "368fdc73bfb14e1c3c1b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe4011b728b070df372c",
+    "url": "/static/js/2.949dba5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.949dba5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "368fdc73bfb14e1c3c1b",
+    "url": "/static/js/main.f8dddd4a.chunk.js"
+  },
+  {
+    "revision": "4d7d166e5fb026251dca",
+    "url": "/static/js/runtime-main.e5baa8fc.js"
+  }
+]);
--- shopping/build/service-worker.js
+++ shopping/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.415dd9fc7ac8e264562d3ef4ed87e440.js"
+  "/precache-manifest.b616d2e24fbcb4a46c1cade4fca367a7.js"
 );
 
 self.addEventListener('message', (event) => {
