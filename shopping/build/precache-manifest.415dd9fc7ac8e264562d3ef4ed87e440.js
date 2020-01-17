d50c80470e27608b541b1499037ed3e58ef43e8d
Karen Bao <karenbao2021@u.northwestern.edu>
2020-01-16 19:52:02 -0600

    second commit

---
+++ shopping/build/precache-manifest.415dd9fc7ac8e264562d3ef4ed87e440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186a0f0ccd8782d60fd242ef95cc18de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f452193e55bd5dcd9b2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81e7f5b7b7c62b50950c",
+    "url": "/static/js/2.205ee4c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.205ee4c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f452193e55bd5dcd9b2",
+    "url": "/static/js/main.1d01e0c9.chunk.js"
+  },
+  {
+    "revision": "4d7d166e5fb026251dca",
+    "url": "/static/js/runtime-main.e5baa8fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
